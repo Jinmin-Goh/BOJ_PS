82b75e1de119912433d512aa7b74a69ec1460701
Jinmin Goh <eric970901@gmail.com>
2019-11-25 00:56:33 +0900

    problem 2557 solution source code

---
+++ Solved/02557/02557.cpp
@@ -0,0 +1,12 @@
+// Problem No.: 02557
+// Solver:      Jinmin Goh
+// Date:        191119
+// URL: https://www.acmicpc.net/problem/2557
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
