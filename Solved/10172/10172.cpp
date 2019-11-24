eb1e2d8154fecf5631b28b0b77031f73fdfca0cd
Jinmin Goh <eric970901@gmail.com>
2019-11-25 00:58:06 +0900

    problem 10172 solution source code

---
+++ Solved/10172/10172.cpp
@@ -0,0 +1,12 @@
+// Problem No.: 10172
+// Solver:      Jinmin Goh
+// Date:        191119
+// URL: https://www.acmicpc.net/problem/10172
+
+#include <stdio.h>
+
+int main()
+{
+    printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+    return 0;
+}
