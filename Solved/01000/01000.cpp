f983265721897cf0534f861673a4f2c6f132c5e5
Jinmin Goh <eric970901@gmail.com>
2019-11-25 00:55:11 +0900

    problem 1000 solution source code

---
+++ Solved/01000/01000.cpp
@@ -0,0 +1,14 @@
+// Problem No.: 01000
+// Solver:      Jinmin Goh
+// Date:        191119
+// URL: https://www.acmicpc.net/problem/1000
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a+b);
+    return 0;
+}
