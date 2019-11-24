ef8a0f7728ba204dd8191a960d32560b48bfd4fa
Jinmin Goh <eric970901@gmail.com>
2019-11-25 00:55:19 +0900

    problem 1001 solution source code

---
+++ Solved/01001/01001.cpp
@@ -0,0 +1,14 @@
+// Problem No.: 01001
+// Solver:      Jinmin Goh
+// Date:        191119
+// URL: https://www.acmicpc.net/problem/1001
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a-b);
+    return 0;
+}
