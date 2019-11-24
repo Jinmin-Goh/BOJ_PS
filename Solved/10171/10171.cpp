205507a3e80f955a73c161d7c4a17fae8a64401e
Jinmin Goh <eric970901@gmail.com>
2019-11-25 00:57:58 +0900

    problem 10171 solution source code

---
+++ Solved/10171/10171.cpp
@@ -0,0 +1,12 @@
+// Problem No.: 10171
+// Solver:      Jinmin Goh
+// Date:        191119
+// URL: https://www.acmicpc.net/problem/10171
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+    return 0;
+}
