80e99f7fbc0d84c5d5ddbea66659c456a68bc17c
Jinmin Goh <eric970901@gmail.com>
2019-11-25 00:59:21 +0900

    problem 10998 solution source code

---
+++ Solved/10998/10998.cpp
@@ -0,0 +1,14 @@
+// Problem No.: 10998
+// Solver:      Jinmin Goh
+// Date:        191119
+// URL: https://www.acmicpc.net/problem/10998
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a*b);
+    return 0;
+}
