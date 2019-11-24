d0a7b86406ebe9a2737b6e37167201e626d45c41
Jinmin Goh <eric970901@gmail.com>
2019-11-25 00:58:24 +0900

    problem 10718 solution source code

---
+++ Solved/10718/10718.cpp
@@ -0,0 +1,12 @@
+// Problem No.: 10718
+// Solver:      Jinmin Goh
+// Date:        191119
+// URL: https://www.acmicpc.net/problem/10718
+
+#include <stdio.h>
+
+int main()
+{
+    printf("강한친구 대한육군\n강한친구 대한육군");
+    return 0;
+}
