c060efd6e654a6c7a3a2205b88c70e000539744c
Jinmin Goh <eric970901@gmail.com>
2019-11-25 00:57:30 +0900

    problem 7287 solution source code

---
+++ Solved/07287/07287.cpp
@@ -0,0 +1,12 @@
+// Problem No.: 07287
+// Solver:      Jinmin Goh
+// Date:        191119
+// URL: https://www.acmicpc.net/problem/7287
+
+#include <stdio.h>
+
+int main()
+{
+    printf("8\neric9709");
+    return 0;
+}
