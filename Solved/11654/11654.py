d69b2f43cd75df72daee0d1553b1421365fb5c58
Jinmin Goh <eric970901@gmail.com>
2020-03-08 23:47:18 +0900

    problem 11654 solution source code

---
+++ Solved/11654/11654.py
@@ -0,0 +1,12 @@
+# Problem No.: 11654
+# Solver:      Jinmin Goh
+# Date:        20200308
+# URL: https://www.acmicpc.net/problem/11654
+
+import sys
+
+def main():
+    print(ord(input()))
+
+if __name__ == "__main__":
+    main()
