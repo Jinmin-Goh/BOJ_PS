a453cf8434f6ceada8a9adced81e038edafa42e2
Jinmin Goh <eric970901@gmail.com>
2020-03-13 02:52:38 +0900

    problem 2163 solution source code

---
+++ Solved/02163/02163.py
@@ -0,0 +1,14 @@
+# Problem No.: 2163
+# Solver:      Jinmin Goh
+# Date:        20200312
+# URL: https://www.acmicpc.net/problem/2163
+
+import sys
+
+def main():
+    n, m = map(int, input().split())
+    print(n * m - 1)
+    return
+
+if __name__ == "__main__":
+    main()
