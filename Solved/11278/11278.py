1121e7ccf35142fbb37da88b022311585b43e42a
Jinmin Goh <eric970901@gmail.com>
2020-05-15 08:23:59 +0900

    problem 11278 solution source code

---
+++ Solved/11278/11278.py
@@ -0,0 +1,44 @@
+# Problem No.: 11277
+# Solver:      Jinmin Goh
+# Date:        20200515
+# URL: https://www.acmicpc.net/problem/11277
+
+import sys
+
+def main():
+    n, m = map(int, input().split())
+    numList = []
+    for _ in range(m):
+        temp = list(map(int, input().split()))
+        numList.append(temp)
+    cnt = [False] * n
+    for i in range(2 ** n):
+        temp = True
+        for j in numList:
+            a = cnt[abs(j[0]) - 1]
+            b = cnt[abs(j[1]) - 1]
+            if j[0] < 0:
+                a = not a 
+            if j[1] < 0:
+                b = not b
+            temp = temp and (a or b)
+            if not temp:
+                break
+        if temp:
+            print(1)
+            for i in cnt:
+                print(int(i), end = " ")
+            return
+        tempCnt = 0
+        while tempCnt < n:
+            cnt[tempCnt] = not cnt[tempCnt]
+            if not cnt[tempCnt]:
+                tempCnt += 1
+            else:
+                break
+
+    print(0)
+    return
+
+if __name__ == "__main__":
+    main()
