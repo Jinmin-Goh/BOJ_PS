686049acdd7d4913521743fea692ea011d84fb4c
Jinmin Goh <eric970901@gmail.com>
2020-05-26 01:45:17 +0900

    problem 8437 int used (naive) solution source code

---
+++ Solved/08437/08437_int.py
@@ -0,0 +1,16 @@
+# Problem No.: 8437
+# Solver:      Jinmin Goh
+# Date:        20200525
+# URL: https://www.acmicpc.net/problem/8437
+
+import sys
+
+def main():
+    a = int(input())
+    b = int(input())
+    print((a + b) // 2)
+    print((a - b) // 2)
+    return
+
+if __name__ == "__main__":
+    main()
