39c2a93079129b2d8ce70de592dd1ab4b5d03830
Jinmin Goh <eric970901@gmail.com>
2020-06-06 13:59:29 +0900

    problem 1676 solution source code

---
+++ Solved/01676/01676.py
@@ -0,0 +1,14 @@
+# Problem No.: 1676
+# Solver:      Jinmin Goh
+# Date:        20200606
+# URL: https://www.acmicpc.net/problem/1676
+
+import sys
+
+def main():
+    n = int(input())
+    print(n // 5 + n // 25 + n // 125)
+    return
+
+if __name__ == "__main__":
+    main()
