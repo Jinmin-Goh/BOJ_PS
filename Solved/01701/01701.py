04f6ae2aa02103874a5806cc1edeb4f826ed98e6
Jinmin Goh <eric970901@gmail.com>
2020-06-23 04:06:52 +0900

    problem 1701 solution source code

---
+++ Solved/01701/01701.py
@@ -0,0 +1,50 @@
+# Problem No.: 1701
+# Solver:      Jinmin Goh
+# Date:        20200622
+# URL: https://www.acmicpc.net/problem/1701
+
+import sys
+import collections
+
+def main():
+    S = input()
+
+    # appropriate prime
+    modVal = 2147483647
+    powVal = 256
+
+    def RabinKarp() -> bool:
+        if pMid == 0:
+            return True
+        tempVal = 0
+        for i in range(pMid):
+            tempVal = (tempVal * powVal + ord(S[i])) % modVal
+
+        mulVal = (1 << (8 * (pMid - 1))) % modVal
+                    
+        hashDict = collections.defaultdict(list)
+        hashDict[tempVal].append(0)
+        
+        for i in range(len(S) - pMid):
+            tempVal = ((tempVal - ord(S[i]) * mulVal) * powVal + ord(S[i + pMid])) % modVal
+            for j in hashDict[tempVal]:
+                if S[i + 1:i + pMid + 1] == S[j:j + pMid]:
+                    return True
+            hashDict[tempVal].append(i + 1)
+        return False
+
+    # binary search
+    pFront = 0
+    pRear = len(S)
+    while pFront + 1 < pRear:
+        pMid = (pFront + pRear) // 2    
+        flag = RabinKarp()
+        if flag:
+            pFront = pMid
+        else:
+            pRear = pMid
+    print(pFront)
+    return
+
+if __name__ == "__main__":
+    main()
