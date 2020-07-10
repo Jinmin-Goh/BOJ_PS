a09b86f9ba3491f32e12af9557230a143a085b65
Jinmin Goh <eric970901@gmail.com>
2020-07-10 17:42:24 +0900

    problem 18322 solution source code

---
+++ Solved/18322/18322.py
@@ -0,0 +1,23 @@
+# Problem No.: 18322
+# Solver:      Jinmin Goh
+# Date:        20200710
+# URL: https://www.acmicpc.net/problem/18322
+
+import sys
+
+def main():
+    n, k = map(int, input().split())
+    words = sys.stdin.readline().split()
+    words = list(reversed(words))
+    cnt = 0
+    while words:
+        temp = words.pop()
+        if cnt + len(temp) > k:
+            print("")
+            cnt = 0
+        cnt += len(temp)
+        print(temp, end = " ")
+    return
+
+if __name__ == "__main__":
+    main()
