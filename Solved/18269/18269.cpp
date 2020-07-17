9292aa96215623e4151a70ba1152f4d9fbe9373d
Jinmin Goh <eric970901@gmail.com>
2020-07-17 14:28:21 +0900

    problem 18269 solution source code

--- Solved/18269/18269.cpp
+++ Solved/18269/18269.cpp
@@ -27,28 +27,31 @@ using namespace std;
 int main(){
     int n;
     scanf("%d", &n);
-    char str[101];
+    char str[101] = {};
     scanf("%s", str);
 
     int ans = n - 1;
     // string checking part; NEED TO FIX; add one more loop
     while(ans > 0){
         bool sameFlag = true;  // check substring is same
-        // i is distance between two substrings
-        for(int i = 1; i <= n - ans; i++){
-            int p = 0;
-            printf("%d %d %d %c %c\n", ans, i, p, str[p], str[p + i]);
-            while(p < ans && str[p] == str[p + i]){
-                printf("same: %d %d %d %c %c\n", ans, i, p, str[p], str[p + i]);
-                p++;
+        for(int start = 0; start < n - ans; start++){
+            // i is distance between two substrings
+            for(int i = start + 1; i <= n - ans; i++){
+                int p = 0;
+                while(p < ans && str[start + p] == str[p + i]){
+                    p++;
+                }
+                if(p != ans){   // same substring
+                    sameFlag = false;   
+                }
+                else{           // different substring
+                    sameFlag = true;   
+                }
+                // if two substring is same; found minimum value, break
+                if(sameFlag){
+                    break;
+                }
             }
-            if(p != ans){   // same substring
-                sameFlag = false;   
-            }
-            else{           // different substring
-                sameFlag = true;   
-            }
-            // if two substring is same; found minimum value, break
             if(sameFlag){
                 break;
             }
