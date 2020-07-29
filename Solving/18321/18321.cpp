ea7717e2ea7d9a5bac99250ee979f2a58c2e9745
Jinmin Goh <eric970901@gmail.com>
2020-07-29 18:40:30 +0900

    18321 / C++ / union-find part implementing

--- Solving/18321/18321.cpp
+++ Solving/18321/18321.cpp
@@ -26,26 +26,42 @@ using namespace std;
 
 int main() {
     int n, m, cows[100010], a[100010], b[100010], w[100010];
+    vector<vector<int>> edge;
     bool flag = true;
     // parsing
     scanf("%d %d", &n, &m);
     for(int i = 0; i < n; i++) {
-        int temp;
-        scanf("%d", &temp);
-        if(cows[i] != temp) {
-            if(cows[i] != i + 1) {
-                flag = false
-            }
+        scanf("%d", &cows[i]);
+        if(cows[i] != i + 1) {
+            flag = false;
         }
     }
     for(int i = 0; i < m; i++) {
         scanf("%d %d %d", &a[i], &b[i], &w[i]);
+        edge.push_back(vector<int> {w[i], a[i], b[i]});
     }
-    
+    sort(edge.begin(), edge.end());
+
     // no need to change
     if(flag) {
         printf("%d", -1);
-        return
+        return 0;
+    }
+
+    // do binary search for answer
+    int left = 0, right = m - 1;
+    while(left < right) {
+        int mid = (left + right + 1) / 2;
+        bool connectFlag = true;
+
+        // do union(for connected components) - find(check whether all different components are connected)
+
+        if(connectFlag) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
     }
     
     return 0;
