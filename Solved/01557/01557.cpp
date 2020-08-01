63c07c4a019bce9bc5de70e6ed9b541224add863
Jinmin Goh <eric970901@gmail.com>
2020-08-02 03:04:02 +0900

    problem 1557 solution source code; refactoring processed

--- Solved/01557/01557.cpp
+++ Solved/01557/01557.cpp
@@ -25,13 +25,11 @@
 #include <unordered_set>
 using namespace std;
 
-#define int long long
+long long int mobiusList[1000100];
 
-int mobiusList[1010100];
-
-int countNonSquare(int N) { 
-    int ans = 0; 
-    for (int i = 1; i * i <= N; i++) {
+long long int countNonSquare(long long int N) { 
+    long long int ans = 0; 
+    for (long long int i = 1; i * i <= N; i++) {
         ans += mobiusList[i] * (N / (i * i));
     }
     return ans;
@@ -39,43 +37,46 @@ int countNonSquare(int N) {
 
 int32_t main() {
     // constructing mobius function
-    for (int i = 1; i <= 1000000; i++) 
+    for (long long int i = 1; i <= 1000000; i++) 
         mobiusList[i] = 1; 
-    for (int i = 2; i * i <= 1000000; i++) {
+    for (long long int i = 2; i * i <= 1000000; i++) {
         if (mobiusList[i] == 1) { 
-            for (int j = i; j <= 1000000; j += i) {
+            for (long long int j = i; j <= 1000000; j += i) {
                 mobiusList[j] *= -i;
             }
-            for (int j = i * i; j <= 1000000; j += i * i) {
+            for (long long int j = i * i; j <= 1000000; j += i * i) {
                 mobiusList[j] = 0;
             }
         } 
     } 
-    for (int i = 2; i <= 1000000; i++) {
+    for (long long int i = 2; i <= 1000000; i++) {
+        // even primes
         if (mobiusList[i] == i) {
             mobiusList[i] = 1;
         }
+        // odd primes
         else if (mobiusList[i] == -i) {
             mobiusList[i] = -1;
         }
+        // even primes; one prime was bigger than sqrt(1e6)
         else if (mobiusList[i] < 0) {
             mobiusList[i] = 1;
         }
+        // odd primes; one prime was bigger than sqrt(1e6)
         else if (mobiusList[i] > 0) {
             mobiusList[i] = -1;
         }
     }
 
     // parsing
-    int k; 
+    long long int k; 
     scanf("%lld", &k);
     
     // binary search
-    int minVal = 0, maxVal = 1e12; 
+    long long int minVal = 0, maxVal = 1e12; 
     while (minVal + 1 < maxVal) { 
-        int mid = (minVal + maxVal) / 2; 
+        long long int mid = (minVal + maxVal) / 2; 
         if (countNonSquare(mid) < k) minVal = mid; else maxVal = mid;
     } 
     printf("%lld", maxVal);
-
 }
