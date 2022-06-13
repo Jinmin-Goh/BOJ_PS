197f825f05db1ef755e31f8aaaee6188862cf5eb
DESKTOP-61K0FGE\Jinmin Goh <eric970901@gmail.com>
2022-06-14 01:29:50 +0900

    add problem 09251 solution source code

--- Solved/09251/09251.py
+++ Solved/09251/09251.py
@@ -1,6 +1,6 @@
 # Problem No.: 9251
 # Solver:      Jinmin Goh
-# Date:        20200310
+# Date:        20220614
 # URL: https://www.acmicpc.net/problem/9251
 
 import sys
@@ -8,23 +8,17 @@ import sys
 def main():
     word1 = input()
     word2 = input()
-    dpTable = [[0] * len(word2) for i in range(len(word1))]
-    for i in range(len(word1)):
-        for j in range(len(word2)):
-            if word1[i] == word2[j]:
-                if j == 0:
-                    dpTable[i][j] = 1
-                else:
-                    dpTable[i][j] = max(dpTable[i - 1][:j]) + 1
+    dpTable = [[0 for _ in range(len(word2) + 1)] for i in range(len(word1) + 1)]
+    ans = 0
+    for i in range(1, len(word1) + 1):
+        for j in range(1, len(word2) + 1):
+            if word1[i - 1] == word2[j - 1]:
+                dpTable[i][j] = dpTable[i - 1][j - 1] + 1
+                if ans < dpTable[i][j]:
+                    ans = dpTable[i][j]
             else:
-                if j == 0:
-                    if i == 0:
-                        dpTable[i][j] = 0
-                    else:
-                        dpTable[i][j] = dpTable[i - 1][j]
-                else:
-                    dpTable[i][j] = max(dpTable[i][j - 1], dpTable[i - 1][j])
-    print(dpTable[-1][-1])
+                dpTable[i][j] = max(dpTable[i][j - 1], dpTable[i - 1][j])
+    print(ans)
     return
 
 if __name__ == "__main__":
