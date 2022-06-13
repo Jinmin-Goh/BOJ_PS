c5c42fbb8e2677f1086cd04b784bfb0261caf0a1
DESKTOP-61K0FGE\Jinmin Goh <eric970901@gmail.com>
2022-06-14 00:11:15 +0900

    add problem 02206 solution source code

--- Solved/02206/02206.py
+++ Solved/02206/02206.py
@@ -1,6 +1,6 @@
 # Problem No.: 2206
 # Solver:      Jinmin Goh
-# Date:        20200604
+# Date:        20220613
 # URL: https://www.acmicpc.net/problem/2206
 
 import sys
@@ -15,51 +15,57 @@ def main():
     
     # distance table
     distance = [[[-1, -1] for _ in range(m)] for __ in range(n)]
-    distance[0][0] = [1, 1]
+    distance[0][0] = [1, -1]
     
     # BFS
-    queue = [((0, 0), False)]
+    queue = [((0, 0), 0)]
     while queue:
-        temp = queue.pop(0)
-        row = temp[0][0]
-        col = temp[0][1]
-        blockFlag = temp[1]
-        if row > 0:
-            if table[row - 1][col] == "0":
-                if distance[row - 1][col][blockFlag] == -1:
-                    distance[row - 1][col][blockFlag] = distance[row][col][blockFlag] + 1
-                    queue.append(((row - 1, col), blockFlag))
-            else:
-                if not blockFlag:
-                    distance[row - 1][col][1] = distance[row][col][0] + 1
-                    queue.append(((row - 1, col), True))
-        if row < n - 1:
-            if table[row + 1][col] == "0":
-                if distance[row + 1][col][blockFlag] == -1:
-                    distance[row + 1][col][blockFlag] = distance[row][col][blockFlag] + 1
-                    queue.append(((row + 1, col), blockFlag))
-            else:
-                if not blockFlag:
-                    distance[row + 1][col][1] = distance[row][col][0] + 1
-                    queue.append(((row + 1, col), True))
-        if col > 0:
-            if table[row][col - 1] == "0":
-                if distance[row][col - 1][blockFlag] == -1:
-                    distance[row][col - 1][blockFlag] = distance[row][col][blockFlag] + 1
-                    queue.append(((row, col - 1), blockFlag))
-            else:
-                if not blockFlag:
-                    distance[row][col - 1][1] = distance[row][col][0] + 1
-                    queue.append(((row, col - 1), True))
-        if col < m - 1:
-            if table[row][col + 1] == "0":
-                if distance[row][col + 1][blockFlag] == -1:
-                    distance[row][col + 1][blockFlag] = distance[row][col][blockFlag] + 1
-                    queue.append(((row, col + 1), blockFlag))
-            else:
-                if not blockFlag:
-                    distance[row][col + 1][1] = distance[row][col][0] + 1
-                    queue.append(((row, col + 1), True))
+        temp = []
+        for node in queue:
+            row = node[0][0]
+            col = node[0][1]
+            blockFlag = node[1]
+            if row > 0:
+                if table[row - 1][col] == "0":  # path
+                    if distance[row - 1][col][blockFlag] == -1:
+                        distance[row - 1][col][blockFlag] = distance[row][col][blockFlag] + 1
+                        temp.append(((row - 1, col), blockFlag))
+                else:   # wall
+                    if not blockFlag:   # not broke the wall yet
+                        if distance[row - 1][col][1] == -1:     # not visited yet
+                            distance[row - 1][col][1] = distance[row][col][blockFlag] + 1
+                            temp.append(((row - 1, col), 1))
+            if row < n - 1:
+                if table[row + 1][col] == "0":  # path
+                    if distance[row + 1][col][blockFlag] == -1:
+                        distance[row + 1][col][blockFlag] = distance[row][col][blockFlag] + 1
+                        temp.append(((row + 1, col), blockFlag))
+                else:   # wall
+                    if not blockFlag:   # not broke the wall yet
+                        if distance[row + 1][col][1] == -1:     # not visited yet
+                            distance[row + 1][col][1] = distance[row][col][blockFlag] + 1
+                            temp.append(((row + 1, col), 1))
+            if col > 0:
+                if table[row][col - 1] == "0":  # path
+                    if distance[row][col - 1][blockFlag] == -1:
+                        distance[row][col - 1][blockFlag] = distance[row][col][blockFlag] + 1
+                        temp.append(((row, col - 1), blockFlag))
+                else:   # wall
+                    if not blockFlag:   # not broke the wall yet
+                        if distance[row][col - 1][1] == -1:     # not visited yet
+                            distance[row][col - 1][1] = distance[row][col][blockFlag] + 1
+                            temp.append(((row, col - 1), 1))
+            if col < m - 1:
+                if table[row][col + 1] == "0":  # path
+                    if distance[row][col + 1][blockFlag] == -1:
+                        distance[row][col + 1][blockFlag] = distance[row][col][blockFlag] + 1
+                        temp.append(((row, col + 1), blockFlag))
+                else:   # wall
+                    if not blockFlag:   # not broke the wall yet
+                        if distance[row][col + 1][1] == -1:     # not visited yet
+                            distance[row][col + 1][1] = distance[row][col][blockFlag] + 1
+                            temp.append(((row, col + 1), 1))
+        queue = temp
     
     # print phase
     if distance[n - 1][m - 1][0] == -1 and distance[n - 1][m - 1][1] == -1:
