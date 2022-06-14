fe02692053cf02d29c4f9c9fef761999b3d28e16
Jinmin Goh <eric970901@gmail.com>
2022-06-14 13:00:33 +0900

    add problem 02467 solution source code

---
+++ Solved/02467/02467.py
@@ -0,0 +1,38 @@
+# Problem No.: 2467
+# Solver:      Jinmin Goh
+# Date:        20220614
+# URL: https://www.acmicpc.net/problem/2467
+
+import sys
+
+def main():
+    n = int(input())
+    nList = list(input().split())
+    for i in range(n):
+        nList[i] = int(nList[i])
+    p1 = 0
+    p2 = n - 1
+    ans = [p1, p2]
+    minVal = nList[p1] + nList[p2]
+    while p1 + 1 < p2:
+        val1 = nList[p1 + 1] + nList[p2]
+        val2 = nList[p1] + nList[p2 - 1]
+        if abs(minVal) > abs(val1) or abs(minVal) > abs(val2):
+            if abs(val1) < abs(val2):
+                ans = [p1 + 1, p2]
+                minVal = val1
+                p1 += 1
+            else:
+                ans = [p1, p2 - 1]
+                minVal = val2
+                p2 -= 1
+        else:
+            if abs(val1) < abs(val2):
+                p1 += 1
+            else:
+                p2 -= 1
+    print(nList[ans[0]], nList[ans[1]])
+    return
+
+if __name__ == "__main__":
+    main()
