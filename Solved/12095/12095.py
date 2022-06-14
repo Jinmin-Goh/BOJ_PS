7bffa30d5af85609b8bd290b1df7369b2eaf4538
Jinmin Goh <eric970901@gmail.com>
2022-06-14 17:01:46 +0900

    add problem 12095 solution source code

---
+++ Solved/12095/12095.py
@@ -0,0 +1,23 @@
+# Problem No.: 12095
+# Solver:      Jinmin Goh
+# Date:        20220614
+# URL: https://www.acmicpc.net/problem/12095
+
+import sys
+
+def main():
+    print(8, 5, 0, 0, 0, 2, 4, 0, 0)
+    print(7, 2, 0, 0, 0, 0, 0, 0, 9)
+    print(0, 0, 4, 0, 0, 0, 0, 0, 0)
+    print(0, 0, 0, 1, 0, 7, 0, 0, 2)
+    print(3, 0, 5, 0, 0, 0, 9, 0, 0)
+    print(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    print(0, 0, 0, 0, 8, 0, 0, 7, 0)
+    print(0, 1, 7, 0, 0, 0, 0, 0, 0)
+    print(0, 0, 0, 0, 3, 6, 0, 4, 0)
+    
+    
+    return
+
+if __name__ == "__main__":
+    main()
