6b608c5057a7d48a7237985a064ccf1ec3e203ed
Jinmin Goh <eric970901@gmail.com>
2022-06-17 09:36:12 +0900

    add changed problem 10942 solution source code

--- Solved/10942/10942.py
+++ Solved/10942/10942.py
@@ -8,14 +8,14 @@ import sys
 def main():
     n = int(sys.stdin.readline())
     nums = list(map(int, sys.stdin.readline().split()))
-    palindromeSet = set()
+    palindromeSet = [[False for _ in range(n + 1)] for __ in range(n + 1)]
     
     for i in range(n):
         # odd palindrome
         temp = 0
         while i - temp >= 0 and i + temp < n:
             if nums[i - temp] == nums[i + temp]:
-                palindromeSet.add((i - temp, i + temp))
+                palindromeSet[i - temp][i + temp] = True
                 temp += 1
             else:
                 break
@@ -23,14 +23,14 @@ def main():
         temp = 0
         while i - temp >= 0 and i + 1 + temp < n:
             if nums[i - temp] == nums[i + 1 + temp]:
-                palindromeSet.add((i - temp, i + 1 + temp))
+                palindromeSet[i - temp][i + 1 + temp] = True
                 temp += 1
             else:
                 break
     m = int(sys.stdin.readline())
     for i in range(m):
         a, b = map(int, sys.stdin.readline().split())
-        if (a - 1, b - 1) in palindromeSet:
+        if palindromeSet[a - 1][b - 1]:
             print(1)
         else:
             print(0)
