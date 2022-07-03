f0f0155bb001c8412470ad85d9dd1c40ea9d8580
DESKTOP-61K0FGE\Jinmin Goh <eric970901@gmail.com>
2022-07-03 23:34:37 +0900

    add problem 01550 solution source code

---
+++ Solved/01550/01550.py
@@ -0,0 +1,13 @@
+# Problem No.: 1550
+# Solver:      Jinmin Goh
+# Date:        20220703
+# URL: https://www.acmicpc.net/problem/1550
+
+import sys
+
+def main():
+    print(int(input(), 16))
+    return
+
+if __name__ == "__main__":
+    main()
