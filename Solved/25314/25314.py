f2222c04c49ef6897d3446396d4c908cd7461982
Jinmin Goh <eric970901@gmail.com>
2022-07-04 09:39:01 +0900

    add problem 25314 solution source code

---
+++ Solved/25314/25314.py
@@ -0,0 +1,14 @@
+# Problem No.: 25314
+# Solver:      Jinmin Goh
+# Date:        20220704
+# URL: https://www.acmicpc.net/problem/25314
+
+import sys
+
+def main():
+    n = int(input())
+    print('long ' * (n // 4) + 'int')
+    return
+
+if __name__ == "__main__":
+    main()
