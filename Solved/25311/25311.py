1e1d81ce9f0d6cf62c2eaeb3c298db69fc618707
Jinmin Goh <eric970901@gmail.com>
2022-07-04 13:33:56 +0900

    add problem 25311 solution source code

---
+++ Solved/25311/25311.py
@@ -0,0 +1,13 @@
+# Problem No.: 25311
+# Solver:      Jinmin Goh
+# Date:        20220704
+# URL: https://www.acmicpc.net/problem/25311
+
+import sys
+
+def main():
+    print('A')
+    return
+
+if __name__ == "__main__":
+    main()
