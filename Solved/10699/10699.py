6b18e76bae4f82eb85d8c66444fe64684aaec88c
Jinmin Goh <jinmin.goh@ngine.co.kr>
2022-07-13 15:32:30 +0900

    add problem 10699 solution source code

---
+++ Solved/10699/10699.py
@@ -0,0 +1,13 @@
+# Problem No.: 10699
+# Solver:      Jinmin Goh
+# Date:        20220713
+# URL: https://www.acmicpc.net/problem/10699
+
+import sys
+
+def main():
+    print('2022-07-13')
+    return
+
+if __name__ == "__main__":
+    main()
