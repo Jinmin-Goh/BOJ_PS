325e111d7f1e84716802b1a1f1d3d52fa28809ac
Jinmin Goh <jinmin.goh@ngine.co.kr>
2022-07-15 10:19:59 +0900

    add problem 25083 solution source code

---
+++ Solved/25083/25083.py
@@ -0,0 +1,18 @@
+# Problem No.: 25083
+# Solver:      Jinmin Goh
+# Date:        20220715
+# URL: https://www.acmicpc.net/problem/25083
+
+import sys
+
+def main():
+    print('         ,r\'\"7')
+    print('r`-_   ,\'  ,/')
+    print(' \. \". L_r\'')
+    print('   `~\/')
+    print('      |')
+    print('      |')
+    return
+
+if __name__ == "__main__":
+    main()
