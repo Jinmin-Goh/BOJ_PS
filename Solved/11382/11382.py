1718002608b489c23a6030ab43414d44b912711d
Jinmin Goh <eric970901@gmail.com>
2022-07-17 13:47:38 +0900

    add problem 11382 solution source code

---
+++ Solved/11382/11382.py
@@ -0,0 +1,13 @@
+# Problem No.: 11382
+# Solver:      Jinmin Goh
+# Date:        20220717
+# URL: https://www.acmicpc.net/problem/11382
+
+import sys
+
+def main():
+    print(sum(map(int, input().split())))
+    return
+
+if __name__ == "__main__":
+    main()
