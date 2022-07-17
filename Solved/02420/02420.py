ad460fc6d6dbaeeb36fd846956d4c50e698e0847
Jinmin Goh <eric970901@gmail.com>
2022-07-17 13:49:59 +0900

    add problem 02420 solution source code

---
+++ Solved/02420/02420.py
@@ -0,0 +1,14 @@
+# Problem No.: 2420
+# Solver:      Jinmin Goh
+# Date:        20220717
+# URL: https://www.acmicpc.net/problem/2420
+
+import sys
+
+def main():
+    a, b = map(int, input().split())
+    print(abs(a - b))
+    return
+
+if __name__ == "__main__":
+    main()
