d27ffb7ae18bf0b964909c34788e020a47653f26
Jinmin Goh <jinmin.goh@ngine.co.kr>
2022-07-21 09:56:28 +0900

    add problem 11280 solution source code

---
+++ Solved/11280/11280.py
@@ -0,0 +1,68 @@
+# Problem No.: 11280
+# Solver:      Jinmin Goh
+# Date:        20220720
+# URL: https://www.acmicpc.net/problem/11280
+
+import sys
+
+sys.setrecursionlimit(10 ** 5)
+
+def dfs(graph, visited, i, stack):
+    if visited[i]:
+        return
+    visited[i] = True
+    for node in graph[i]:
+        if visited[node]:
+            continue
+        dfs(graph, visited, node, stack)
+    stack.append(i)
+
+def dfs2(graph, visited, i, stack):
+    if visited[i]:
+        return
+    visited[i] = True
+    stack.append(i)
+    for node in graph[i]:
+        if visited[node]:
+            continue
+        dfs2(graph, visited, node, stack)
+
+def main():
+    n, m = map(int, sys.stdin.readline().split())
+    graph = [[] for _ in range(2 * n)]
+    rev_graph = [[] for _ in range(2 * n)]
+    for _ in range(m):
+        a, b = map(int, sys.stdin.readline().split())
+        a = a * 2 - 1 if a > 0 else (-a - 1) * 2
+        b = b * 2 - 1 if b > 0 else (-b - 1) * 2
+        graph[a - 1 if a % 2 else a + 1].append(b)
+        graph[b - 1 if b % 2 else b + 1].append(a)
+        rev_graph[b].append(a - 1 if a % 2 else a + 1)
+        rev_graph[a].append(b - 1 if b % 2 else b + 1)
+    stack = []
+    visited = [False for _ in range(2 * n)]
+    for i in range(2 * n):
+        dfs(graph, visited, i, stack)
+    visited = [False for _ in range(2 * n)]
+    group = [-1 for _ in range(2 * n)]
+    cnt = 0
+    while stack:
+        node = stack.pop()
+        if visited[node]:
+            continue
+        temp = []
+        dfs2(rev_graph, visited, node, temp)
+        for i in temp:
+            group[i] = cnt
+        cnt += 1
+    for i in range(n):
+        if group[i * 2] == group[i * 2 + 1]:
+            print(0)
+            return
+    print(1)
+
+    return
+
+if __name__ == "__main__":
+    main()
+    
