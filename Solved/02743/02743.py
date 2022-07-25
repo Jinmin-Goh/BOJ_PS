db812b5960682b67b277146dc1c6a4e7cd05a2e2
Jinmin Goh <jinmin.goh@ngine.co.kr>
2022-07-25 10:35:12 +0900

    add problem 02743 solution source code

---
+++ Solved/02743/02743.py
@@ -0,0 +1,13 @@
+# Problem No.: 2743
+# Solver:      Jinmin Goh
+# Date:        20220725
+# URL: https://www.acmicpc.net/problem/2743
+
+import sys
+
+def main():
+    print(len(input()))
+    return
+
+if __name__ == "__main__":
+    main()
