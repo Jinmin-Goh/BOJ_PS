a78eaa616aeb5eef67a3de0abb94b5c48e7995cb
Jinmin Goh <jinmin.goh@ngine.co.kr>
2022-07-27 17:45:11 +0900

    add problem 11718 solution source code

---
+++ Solved/11718/11718.py
@@ -0,0 +1,13 @@
+# Problem No.: 11718
+# Solver:      Jinmin Goh
+# Date:        20220727
+# URL: https://www.acmicpc.net/problem/11718
+
+import sys
+
+def main():
+    print(sys.stdin.read()) 
+    return
+
+if __name__ == "__main__":
+    main()
