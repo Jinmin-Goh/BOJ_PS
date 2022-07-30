7979a4c5525c01492bd5753fbdce382290454d69
Jinmin Goh <eric970901@gmail.com>
2022-07-30 16:54:32 +0900

    add problem 15964 solution source code

---
+++ Solved/15964/15964.py
@@ -0,0 +1,14 @@
+# Problem No.: 15964
+# Solver:      Jinmin Goh
+# Date:        20220730
+# URL: https://www.acmicpc.net/problem/15964
+
+import sys
+
+def main():
+    a, b = map(int, input().split())
+    print(a * a - b * b)
+    return
+
+if __name__ == "__main__":
+    main()
