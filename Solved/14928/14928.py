9bd8cc68806cd1f59c3e5896805d03e579964c64
Jinmin Goh <jinmin.goh@ngine.co.kr>
2022-08-01 10:21:39 +0900

    add problem 14928 solution source code

---
+++ Solved/14928/14928.py
@@ -0,0 +1,13 @@
+# Problem No.: 14928
+# Solver:      Jinmin Goh
+# Date:        20220801
+# URL: https://www.acmicpc.net/problem/14928
+
+import sys
+
+def main():
+    print(int(input()) % 20000303)
+    return
+
+if __name__ == "__main__":
+    main()
