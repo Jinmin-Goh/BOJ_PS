33d1c38caa4a001563c72b84c7b3f9a985504983
Jinmin Goh <jinmin.goh@ngine.co.kr>
2022-08-02 13:46:30 +0900

    add problem 15727 solution source code

---
+++ Solved/15727/15727.py
@@ -0,0 +1,13 @@
+# Problem No.: 15727
+# Solver:      Jinmin Goh
+# Date:        20220802
+# URL: https://www.acmicpc.net/problem/15727
+
+import sys
+
+def main():
+    print((int(input()) - 1) // 5 + 1)
+    return
+
+if __name__ == "__main__":
+    main()
