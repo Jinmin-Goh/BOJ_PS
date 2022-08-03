7f49714bf3d9976e263048ff1ae26a5993b6424e
Jinmin Goh <jinmin.goh@ngine.co.kr>
2022-08-03 09:31:28 +0900

    add problem 18108 solution source code

---
+++ Solved/18108/18108.py
@@ -0,0 +1,13 @@
+# Problem No.: 18108
+# Solver:      Jinmin Goh
+# Date:        20220803
+# URL: https://www.acmicpc.net/problem/18108
+
+import sys
+
+def main():
+    print(int(input()) - 543)
+    return
+
+if __name__ == "__main__":
+    main()
