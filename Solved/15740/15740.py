987f0eb4ee12876b927b38294bc0a74e0701b953
Jinmin Goh <eric970901@gmail.com>
2022-08-04 10:02:09 +0900

    add problem 15740 solution source code

---
+++ Solved/15740/15740.py
@@ -0,0 +1,13 @@
+# Problem No.: 15740
+# Solver:      Jinmin Goh
+# Date:        20220804
+# URL: https://www.acmicpc.net/problem/15740
+
+import sys
+
+def main():
+    print(sum(map(int, sys.stdin.readline().split())))
+    return
+
+if __name__ == "__main__":
+    main()
