39bf767fb9dd96c05033cfb7dac4c656a8eeaebb
Jinmin Goh <eric970901@gmail.com>
2022-08-06 22:35:47 +0900

    add problem 05339 solution source code

---
+++ Solved/05339/05339.py
@@ -0,0 +1,22 @@
+# Problem No.: 5339
+# Solver:      Jinmin Goh
+# Date:        20220806
+# URL: https://www.acmicpc.net/problem/5339
+
+import sys
+
+def main():
+    print("     /~\\\n\
+    ( oo|\n\
+    _\=/_\n\
+   /  _  \\\n\
+  //|/.\|\\\\\n\
+ ||  \ /  ||\n\
+============\n\
+|          |\n\
+|          |\n\
+|          |")
+    return
+
+if __name__ == "__main__":
+    main()
