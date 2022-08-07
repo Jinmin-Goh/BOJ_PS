12797a5d26be5a0bb0f4000b44cb9374b49a6a89
Jinmin Goh <eric970901@gmail.com>
2022-08-07 16:03:20 +0900

    add problem 24078 solution source code

---
+++ Solved/24078/24078.py
@@ -0,0 +1,13 @@
+# Problem No.: 24078
+# Solver:      Jinmin Goh
+# Date:        20220807
+# URL: https://www.acmicpc.net/problem/24078
+
+import sys
+
+def main():
+    print(int(input()) % 21)
+    return
+
+if __name__ == "__main__":
+    main()
