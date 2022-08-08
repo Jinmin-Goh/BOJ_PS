b3f6281765edf547a84f03df46f2fd6de0989c52
Jinmin Goh <eric970901@gmail.com>
2022-08-08 10:09:59 +0900

    add problem 10757 solution source code

---
+++ Solved/10757/10757.py
@@ -0,0 +1,13 @@
+# Problem No.: 10757
+# Solver:      Jinmin Goh
+# Date:        20220808
+# URL: https://www.acmicpc.net/problem/10757
+
+import sys
+
+def main():
+    print(sum(map(int, sys.stdin.readline().split())))
+    return
+
+if __name__ == "__main__":
+    main()
