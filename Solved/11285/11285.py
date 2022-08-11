c99c5e3be93d171d5fc10f5e5564b753ecf2101c
Jinmin Goh <eric970901@gmail.com>
2022-08-11 12:21:02 +0900

    add problem 11285 solution source code

---
+++ Solved/11285/11285.py
@@ -0,0 +1,24 @@
+# Problem No.: 11285
+# Solver:      Jinmin Goh
+# Date:        20220811
+# URL: https://www.acmicpc.net/problem/11285
+
+import sys
+
+def main():
+    first_word = input().rstrip()
+    middle_word = input().rstrip()
+    last_word = input().rstrip()
+    first_list = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    middle_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+    last_list = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    
+    first_idx = first_list.index(first_word)
+    middle_idx = middle_list.index(middle_word)
+    last_idx = last_list.index(last_word)
+    idx = first_idx * (len(middle_list) * len(last_list)) + middle_idx * len(last_list) + last_idx + 44032
+    print(chr(idx))
+    return
+
+if __name__ == "__main__":
+    main()
