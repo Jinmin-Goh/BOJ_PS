f202d6c3e2b848bb9e54971f28dbaca86eb2716b
Jinmin Goh <eric970901@gmail.com>
2022-08-11 11:17:00 +0900

    add problem 10926 solution source code

---
+++ Solved/10926/10926.py
@@ -0,0 +1,13 @@
+# Problem No.: 10926
+# Solver:      Jinmin Goh
+# Date:        20220811
+# URL: https://www.acmicpc.net/problem/10926
+
+import sys
+
+def main():
+    print(input() + "??!")
+    return
+
+if __name__ == "__main__":
+    main()
