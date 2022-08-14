ae8d6ecf7bdff34987825080ad88c09b16a81538
Jinmin Goh <eric970901@gmail.com>
2022-08-14 23:50:22 +0900

    add problem 02338 solution source code

---
+++ Solved/02338/02338.py
@@ -0,0 +1,17 @@
+# Problem No.: 2338
+# Solver:      Jinmin Goh
+# Date:        20220814
+# URL: https://www.acmicpc.net/problem/2338
+
+import sys
+
+def main():
+    a = int(sys.stdin.readline().rstrip())
+    b = int(sys.stdin.readline().rstrip())
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    return
+
+if __name__ == "__main__":
+    main()
