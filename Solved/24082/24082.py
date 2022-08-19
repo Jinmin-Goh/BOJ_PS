bdb28049fc1b53860ce6d106e28fb98184fede1e
Jinmin Goh <eric970901@gmail.com>
2022-08-19 09:58:28 +0900

    add problem 24082 solution source code

---
+++ Solved/24082/24082.py
@@ -0,0 +1,13 @@
+# Problem No.: 24082
+# Solver:      Jinmin Goh
+# Date:        20220819
+# URL: https://www.acmicpc.net/problem/24082
+
+import sys
+
+def main():
+    print(int(input()) ** 3)
+    return
+
+if __name__ == "__main__":
+    main()
