c23bdc639921090f8c9253158bd11c6e027c193d
Jinmin Goh <eric970901@gmail.com>
2022-08-27 03:31:42 +0900

    add problem 05338 solution source code

---
+++ Solved/05338/05338.py
@@ -0,0 +1,17 @@
+# Problem No.: 5338
+# Solver:      Jinmin Goh
+# Date:        20220827
+# URL: https://www.acmicpc.net/problem/5338
+
+import sys
+
+def main():
+    print("       _.-;;-._\n\
+'-..-'|   ||   |\n\
+'-..-'|_.-;;-._|\n\
+'-..-'|   ||   |\n\
+'-..-'|_.-''-._|")
+    return
+
+if __name__ == "__main__":
+    main()
