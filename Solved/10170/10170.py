898c37fc6c1a6a48f743711a49e8bad21f81380b
Jinmin Goh <eric970901@gmail.com>
2022-09-05 18:30:54 +0900

    add problem 10170 solution source code

---
+++ Solved/10170/10170.py
@@ -0,0 +1,25 @@
+# Problem No.: 10170
+# Solver:      Jinmin Goh
+# Date:        20220905
+# URL: https://www.acmicpc.net/problem/10170
+
+import sys
+
+def main():
+    print("NFC West       W   L  T\n\
+-----------------------\n\
+Seattle        13  3  0\n\
+San Francisco  12  4  0\n\
+Arizona        10  6  0\n\
+St. Louis      7   9  0\n\
+\n\
+NFC North      W   L  T\n\
+-----------------------\n\
+Green Bay      8   7  1\n\
+Chicago        8   8  0\n\
+Detroit        7   9  0\n\
+Minnesota      5  10  1")
+    return
+
+if __name__ == "__main__":
+    main()
