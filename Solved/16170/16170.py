66991e29d5254a80874e15ea0bc258c630c4bd31
Jinmin Goh <eric970901@gmail.com>
2022-09-06 15:00:12 +0900

    add problem 16170 solution source code

---
+++ Solved/16170/16170.py
@@ -0,0 +1,13 @@
+# Problem No.: 16170
+# Solver:      Jinmin Goh
+# Date:        20220906
+# URL: https://www.acmicpc.net/problem/16170
+
+import sys
+
+def main():
+    print("2022\n09\n06")
+    return
+
+if __name__ == "__main__":
+    main()
