28c5d093fd0d0af2df391af0d80a8a49f4ff1705
Jinmin Goh <eric970901@gmail.com>
2022-09-07 11:09:20 +0900

    add problem 18096 solution source code

---
+++ Solved/18096/18096.py
@@ -0,0 +1,13 @@
+# Problem No.: 18096
+# Solver:      Jinmin Goh
+# Date:        20220907
+# URL: https://www.acmicpc.net/problem/18096
+
+import sys
+
+def main():
+    print(1)
+    return
+
+if __name__ == "__main__":
+    main()
