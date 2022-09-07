bfa591bc5085ee24e0f5238c23ff9a4860f39c37
Jinmin Goh <eric970901@gmail.com>
2022-09-07 14:32:58 +0900

    add problem 08545 solution source code

---
+++ Solved/08545/08545.py
@@ -0,0 +1,13 @@
+# Problem No.: 8545
+# Solver:      Jinmin Goh
+# Date:        20220907
+# URL: https://www.acmicpc.net/problem/8545
+
+import sys
+
+def main():
+    print(''.join(list(reversed(list(input())))))
+    return
+
+if __name__ == "__main__":
+    main()
