6953b155dcd1c5f3f54d7c5a852256092ea2bfc6
Jinmin Goh <eric970901@gmail.com>
2022-09-07 14:35:15 +0900

    add problem 08871 solution source code

---
+++ Solved/08871/08871.py
@@ -0,0 +1,14 @@
+# Problem No.: 8871
+# Solver:      Jinmin Goh
+# Date:        20220907
+# URL: https://www.acmicpc.net/problem/8871
+
+import sys
+
+def main():
+    n = int(input()) + 1
+    print(2 * n, 3 * n)
+    return
+
+if __name__ == "__main__":
+    main()
