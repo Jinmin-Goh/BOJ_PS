24d0d97e459160a15342fa25c7c92e31ca4627e1
Jinmin Goh <eric970901@gmail.com>
2022-09-07 10:49:36 +0900

    add problem 16394 solution source code

---
+++ Solved/16394/16394.py
@@ -0,0 +1,13 @@
+# Problem No.: 16394
+# Solver:      Jinmin Goh
+# Date:        20220907
+# URL: https://www.acmicpc.net/problem/16394
+
+import sys
+
+def main():
+    print(int(input()) - 1946)
+    return
+
+if __name__ == "__main__":
+    main()
