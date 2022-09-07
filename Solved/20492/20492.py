1fd3711f3e53492c1945d589befc5ceb6c02a1e6
Jinmin Goh <eric970901@gmail.com>
2022-09-07 11:16:14 +0900

    add problem 20492 solution source code

---
+++ Solved/20492/20492.py
@@ -0,0 +1,14 @@
+# Problem No.: 20492
+# Solver:      Jinmin Goh
+# Date:        20220907
+# URL: https://www.acmicpc.net/problem/20492
+
+import sys
+
+def main():
+    n = int(sys.stdin.readline().rstrip())
+    print(int(n * 0.78), int(n * 0.956))
+    return
+
+if __name__ == "__main__":
+    main()
