5298aba17cd26a34d7623c2a5aa9b9cf58e15b0b
Jinmin Goh <eric970901@gmail.com>
2022-09-08 15:23:18 +0900

    add problem 24086 solution source code

---
+++ Solved/24086/24086.py
@@ -0,0 +1,15 @@
+# Problem No.: 24086
+# Solver:      Jinmin Goh
+# Date:        20220908
+# URL: https://www.acmicpc.net/problem/24086
+
+import sys
+
+def main():
+    a = int(sys.stdin.readline().rstrip())
+    b = int(sys.stdin.readline().rstrip())
+    print(b - a)
+    return
+
+if __name__ == "__main__":
+    main()
