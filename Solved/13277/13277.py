dff6d924f100fbfbf2b855191819dbe5f9bd2e5a
Jinmin Goh <eric970901@gmail.com>
2022-09-08 11:10:08 +0900

    add problem 13277 solution source code

---
+++ Solved/13277/13277.py
@@ -0,0 +1,14 @@
+# Problem No.: 13277
+# Solver:      Jinmin Goh
+# Date:        20220908
+# URL: https://www.acmicpc.net/problem/13277
+
+import sys
+
+def main():
+    a, b = map(int, sys.stdin.readline().split())
+    print(a * b)
+    return
+
+if __name__ == "__main__":
+    main()
