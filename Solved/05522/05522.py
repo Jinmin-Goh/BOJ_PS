80e17c0c52c39d2b57d92b782c7a5ce4446a0ee8
Jinmin Goh <eric970901@gmail.com>
2022-09-08 10:39:48 +0900

    add problem 05522 solution source code

---
+++ Solved/05522/05522.py
@@ -0,0 +1,16 @@
+# Problem No.: 5522
+# Solver:      Jinmin Goh
+# Date:        20220908
+# URL: https://www.acmicpc.net/problem/5522
+
+import sys
+
+def main():
+    ans = 0
+    for _ in range(5):
+        ans += int(input())
+    print(ans)
+    return
+
+if __name__ == "__main__":
+    main()
