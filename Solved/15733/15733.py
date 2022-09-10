750dff3faf480792756a1f221f769e7acbead1b3
Jinmin Goh <eric970901@gmail.com>
2022-09-10 13:19:29 +0900

    add problem 15733 solution source code

---
+++ Solved/15733/15733.py
@@ -0,0 +1,13 @@
+# Problem No.: 15733
+# Solver:      Jinmin Goh
+# Date:        20220910
+# URL: https://www.acmicpc.net/problem/15733
+
+import sys
+
+def main():
+    print("I'm Sexy")
+    return
+
+if __name__ == "__main__":
+    main()
