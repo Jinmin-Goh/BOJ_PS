4f8e3c3029c3958313156b82a298813ec57a0912
Jinmin Goh <eric970901@gmail.com>
2022-09-13 16:56:35 +0900

    add problem 11942 solution source code

---
+++ Solved/11942/11942.py
@@ -0,0 +1,13 @@
+# Problem No.: 11942
+# Solver:      Jinmin Goh
+# Date:        20220913
+# URL: https://www.acmicpc.net/problem/11942
+
+import sys
+
+def main():
+    print("고려대학교")
+    return
+
+if __name__ == "__main__":
+    main()
