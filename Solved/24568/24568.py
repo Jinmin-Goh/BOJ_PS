dfb4493c725e1d2fe016c0db590d0a027ef6913f
Jinmin Goh <eric970901@gmail.com>
2022-09-13 17:07:45 +0900

    add problem 24568 solution source code

---
+++ Solved/24568/24568.py
@@ -0,0 +1,15 @@
+# Problem No.: 24568
+# Solver:      Jinmin Goh
+# Date:        20220913
+# URL: https://www.acmicpc.net/problem/24568
+
+import sys
+
+def main():
+    a = int(sys.stdin.readline().rstrip())
+    b = int(sys.stdin.readline().rstrip())
+    print(8 * a + 3 * b - 28)
+    return
+
+if __name__ == "__main__":
+    main()
