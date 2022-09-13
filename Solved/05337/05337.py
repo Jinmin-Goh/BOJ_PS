b44bb29d48510c0c80c744563e94f0521daf8db3
Jinmin Goh <eric970901@gmail.com>
2022-09-13 17:04:39 +0900

    add problem 05337 solution source code

---
+++ Solved/05337/05337.py
@@ -0,0 +1,15 @@
+# Problem No.: 5337
+# Solver:      Jinmin Goh
+# Date:        20220913
+# URL: https://www.acmicpc.net/problem/5337
+
+import sys
+
+def main():
+    print(".  .   .\n\
+|  | _ | _. _ ._ _  _\n\
+|/\|(/.|(_.(_)[ | )(/.")
+    return
+
+if __name__ == "__main__":
+    main()
